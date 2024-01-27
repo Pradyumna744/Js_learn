789e15fae7a1e6186171af87a6f7146c89833fd3
Pradyumna744 <123537079+Pradyumna744@users.noreply.github.com>
2024-01-27 16:24:53 +0000

    commited

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+let name = "paddy";
+let age = 21;
+// let isLogged = flase;
+//Number =>22;
+//string=>""
+//boolean=>true/flase
+//null=>standalone value
+//undefined=>s
+//symbol=.unique
+ 
+//object
+console.log(typeof null ) 
+console.log(typeof undefined)
+//  
+ 
