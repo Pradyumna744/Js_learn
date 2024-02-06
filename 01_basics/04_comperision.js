7822388a959660c9889b3fd7e975698d7a223154
Pradyumna744 <marathepradyumna744@gmail.com>
2024-02-06 17:12:24 +0000

    changed

---
+++ 01_basics/04_comperision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+/*NOTE:- Below type convertion is always avoid don't 
+preffered it as point of view of good programmer*/
+console.log("2" >1);
+console.log("02" > 1);
+
+console.log(null > 0);//null is not coverted it always 1
+console.log(null == 0);
+console.log(null>=0);//null covert int 0
+
+console.log(undefined==0);//undefined is always 0
+console.log(undefined<0);
+console.log(undefined>0);
+
+// (===)-stick check  or checking value at strickly 
+
+console.log("2"===2);
