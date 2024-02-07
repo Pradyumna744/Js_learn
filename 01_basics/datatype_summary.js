6bbd139cc94c3feeaa4d651afbd013e18b77b551
Pradyumna744 <marathepradyumna744@gmail.com>
2024-02-07 17:50:52 +0000

    Datatype_summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,42 @@
+// Premitive
+
+// 7 Types: String,Boolearn,Number,Null,Undefined,Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.5
+
+const isLoggedIn = false
+const outTemp = null//empty not zero
+let Undef ="" //undefiend
+
+const id = Symbol('123')
+const anotherId=Symbol('123')
+console.log(id === anotherId);
+ 
+const BigNumber = 12333454457767568587678667885344234n
+
+
+
+
+
+// Non-peritive {Refereance Type}
+
+//Types:- Array,Objects,Functions
+
+/*Array*/const heros = ["Ironman", "hulk", "thor"];     
+/*OBjects*/
+let obj =
+{
+    name:"Pradyumna",
+    age:22,
+
+}
+ /*Functions*/
+//  IN javascript we treat function ass veriable
+ const myFunction=function () {
+    console.log("Hello World");
+ }
+
+ console.log(typeof outTemp);
+    
+ 
