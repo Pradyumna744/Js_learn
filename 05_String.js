8613ee2d3e5c0d043818256d2e5fc3e01d9f86ca
Pradyumna744 <marathepradyumna744@gmail.com>
2024-02-20 10:45:18 +0000

    aboutStrings/its function

---
+++ 05_String.js
@@ -0,0 +1,35 @@
+const name="hitesh"
+const repoCount =50
+
+// console.log(name +repoCount+"value");
+
+console.log(`Hello my name is ${name} and my repo  count is ${repoCount}`);
+
+//  another type of String Decleration
+ 
+ const gameName = new String('pradyuman');
+
+// console.log(gameName[3]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4);
+// console.log(anotherString);
+
+// const newStringOne = "   hemanshu   ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim );
+
+const url = "https://github.com/Pradyumna744%20"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('danny'))
