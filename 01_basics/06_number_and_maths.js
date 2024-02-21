290049ef156135b36b81031a6888170d0e85945e
Pradyumna744 <marathepradyumna744@gmail.com>
2024-02-21 06:56:39 +0000

    numbers and maths

---
+++ 01_basics/06_number_and_maths.js
@@ -0,0 +1,33 @@
+// const score = 400;
+// console.log(score);
+
+// const balance =new Number(100)
+// console.log(balance);
+ 
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
+
+// const othrtNumber = 123.8093
+// console.log(othrtNumber.toPrecision(4));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'))//convert numerics to localStrings
+
+
+// *********************MATH'S***********************
+
+// console.log(Math);
+
+// console.log(Math.abs(- 4));
+// console.log(Math.random(4.6));//all are the  math functions  which perform math oprations
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.max(4,3,6,8));
+// console.log(Math.min(4,3,6,8));
+  
+// console.log(Math.random());  random() math function used
+// console.log((Math.random()*10)+1);
+
+
+
+ 
