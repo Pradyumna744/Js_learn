40a2515ec595689325a5b8072df11710e99171c4
Pradyumna744 <marathepradyumna744@gmail.com>
2024-02-22 06:57:48 +0000

    about Date and its function

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,34 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);//type of date if object date functions are included in objects
+
+
+// let myCreatedDate = new Date(2023, 0, 23)//always in JavaScript months are start with 0 (zero)
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toString());
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleDateString());
+// console.log(typeof myCreatedDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23,5,3)//format declartion type YYYY-MM-DD-HR-MIN-SS   
+// let myCreatedDate = new Date("24-01-2023")
+let myCreatedDate = new Date("2023-01-14")
+// console.log(myCreatedDate.toLocaleTimeString());
+
+
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+//  //console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getDate()); 
+// console.log(newDate.getDay());//its get time function
+// console.log(newDate.getFullYear());
+// console.log(newDate.getMonth()+1);
+//  `${newDate.getDate()} and the time  is ${newDate.getDay()}`String interpostion concept
+
