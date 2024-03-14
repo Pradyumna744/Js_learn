0e6a26a86332d6bc50a87327ca0e95f8dfbc05b2
Pradyumna744 <marathepradyumna744@gmail.com>
2024-03-14 11:04:13 +0000

    about the scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,12 +1,31 @@
 
 // let c = 300;
-let a = 300;
-if (true) { 
-    let a = 10;
-    const b = 20;
-    console.log("inner",a);
+// let a = 300;
+// if (true) { 
+//     let a = 10;
+//     const b = 20;
+//     console.log("inner",a);
     
-}
-console.log(a);
+// }
+/*in the scopes there is limits*/  
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one()
+{
+    /*the js code  was excute line by line and inside 
+    the resources can't be accessed by the parent function  child can access the values of its parents   */
+    const username = "mayur"
+    
+    function two() {
+   
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two();
+}
+
+one()
